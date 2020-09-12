52f6498606b9da49fe54c9f554f69289971d98c2
Qitian Zhang <HowdySunnie@outlook.com>
2020-09-12 16:11:57 +0800

    Use upper case.

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 export function intToString(value: number) {
-  const suffixes = ["", "k", "m", "b", "t"];
+  const suffixes = ["", "K", "M", "B", "T"];
   const suffixNum = Math.floor(("" + value).length / 3);
   let shortValue = parseFloat(
     (suffixNum != 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(2)
