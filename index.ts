2152795b9fc8d7f1a3507d04c4e28b86019f86d2
Qitian Zhang <HowdySunnie@outlook.com>
2020-10-10 08:32:08 +0800

    Fix format.

--- index.ts
+++ index.ts
@@ -5,72 +5,72 @@ import { sprintf } from "https://deno.land/x/std/fmt/printf.ts";
 const biliApiUrl = `https://api.bilibili.com/x/space/arc/search?mid=${config.uid}&ps=5&pn=1`;
 const gistApiUrl = `https://api.github.com/gists/${config.gistId}`;
 interface VList {
-  comment: number;
-  play: number;
-  title: string;
-  bvid: string;
+    comment: number;
+    play: number;
+    title: string;
+    bvid: string;
 }
 
 async function getVList() {
-  const data = await fetch(biliApiUrl);
-  const json = await data.json();
-  const vList = json.data.list.vlist as VList[];
-  return vList;
+    const data = await fetch(biliApiUrl);
+    const json = await data.json();
+    const vList = json.data.list.vlist as VList[];
+    return vList;
 }
 
 async function getRequestData() {
-  const vList = await getVList();
-  let mdContent = "";
-  vList.forEach((v) => {
-    const title = cutString(v.title, 35);
-    //const url = `https://www.bilibili.com/video/${v.bvid}`;
-    const play = intToString(v.play);
-    const comment = intToString(v.comment);
-    //if (!title.includes("【")) {
-    //  mdContent += "【其他】";
-    //}
-    
-    mdContent += sprintf("%s   ▶️ %s   💬 %s", title, play, comment) + "\n";
-    //mdContent += sprintf("[%s](%s) ▶️: %s 💬: %s", title, url, play, comment) + "\n\n";
-  });
-  return {
-    description: "My Latest BiliBili videos 👇",
-    files: {
-      "latest_videos": { content: mdContent },
-      //"latest_videos.md": { content: mdContent },
-    },
-  };
+    const vList = await getVList();
+    let mdContent = "";
+    vList.forEach((v) => {
+        const title = cutString(v.title, 35);
+        //const url = `https://www.bilibili.com/video/${v.bvid}`;
+        const play = intToString(v.play);
+        const comment = intToString(v.comment);
+        //if (!title.includes("【")) {
+        //    mdContent += "【其他】";
+        //}
+        
+        mdContent += sprintf("%s   ▶️ %s   💬 %s", title, play, comment) + "\n";
+        //mdContent += sprintf("[%s](%s) ▶️: %s 💬: %s", title, url, play, comment) + "\n\n";
+    });
+    return {
+        description: "My Latest BiliBili videos",
+        files: {
+            "latest_videos": { content: mdContent },
+            //"latest_videos.md": { content: mdContent },
+        },
+    };
 }
 
 async function updateGist() {
-  const data = await getRequestData();
-  const response = await fetch(gistApiUrl, {
-    body: JSON.stringify(data),
-    method: "POST",
-    headers: {
-      Authorization: `token ${config.ghToken}`,
-      Accept: "application/json",
-    },
-  });
-  const status = await response.status;
-  if (status === 200) {
-    console.log('Update gist success!');
-  } else {
-    throw new Error("Update gist fail!")
-  }
+    const data = await getRequestData();
+    const response = await fetch(gistApiUrl, {
+        body: JSON.stringify(data),
+        method: "POST",
+        headers: {
+            Authorization: `token ${config.ghToken}`,
+            Accept: "application/json",
+        },
+    });
+    const status = await response.status;
+    if (status === 200) {
+        console.log('Update gist success!');
+    } else {
+        throw new Error("Update gist fail!")
+    }
 }
 
 async function run(func:Function){
-  if (!config.ghToken){
-    throw new Error('Please set GH_TOKEN environment variable first!')
-  }
-  if (!config.gistId){
-    throw new Error('Please edit GIST_ID environment variable first!')
-  }
-  if (!config.uid){
-    throw new Error('Please edit UID environment variable first!')
-  }
-  await func();
+    if (!config.ghToken){
+        throw new Error('Please set GH_TOKEN environment variable first!')
+    }
+    if (!config.gistId){
+        throw new Error('Please edit GIST_ID environment variable first!')
+    }
+    if (!config.uid){
+        throw new Error('Please edit UID environment variable first!')
+    }
+    await func();
 }
 
 await run(updateGist)
